500697a0609bb818207b5a79e2e49ec424e9a60e
Mini <wndavey@hotmail.com>
2024-02-01 15:56:48 +1030

    disable printing move count

--- src/game_logic/player_move.c
+++ src/game_logic/player_move.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 16:04:03 by wdavey            #+#    #+#             */
-/*   Updated: 2023/09/04 12:57:15 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/02/01 15:56:30 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,4 @@
 void	player_move(t_entity *player, t_pos move)
 {
 	player->pos = pos_add(player->pos, move);
-	ft_printf("moves: %d\n", (int)++(player->data));
 }
