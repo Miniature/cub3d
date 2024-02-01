c28a248143f97aa32a9a8e59fadee2b3f386da1c
Mini <wndavey@hotmail.com>
2024-02-01 14:26:26 +1030

    turns out you can use float literals

--- src/entity/entity_create.c
+++ src/entity/entity_create.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 12:09:18 by wdavey            #+#    #+#             */
-/*   Updated: 2024/02/01 13:55:54 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/02/01 14:06:45 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_entity	entity_create(enum e_id id, int x, int y, t_sprite *sprite)
 	e.type = id;
 	e.pos.x = x;
 	e.pos.y = y;
-	e.facing = 0.0;
+	e.facing = 0.0f;
 	e.sprite = sprite;
 	e.data = 0;
 	return (e);
