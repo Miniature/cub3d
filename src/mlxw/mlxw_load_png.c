9b2b2f24d07f553fa81f4098fbde49d09fd8e89e
Mini <wndavey@hotmail.com>
2024-02-01 16:04:50 +1030

    fix png loading

--- src/mlxw/mlxw_load_png.c
+++ src/mlxw/mlxw_load_png.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 14:30:40 by wdavey            #+#    #+#             */
-/*   Updated: 2023/08/30 14:02:41 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/02/01 16:04:26 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ t_mlx_image	mlxw_load_png(void *mlx, char *path)
 {
 	t_mlx_image	img;
 
-	mlx_png_file_to_image(mlx, path, &(img.w), &(img.h));
+	img.img = mlx_png_file_to_image(mlx, path, &(img.w), &(img.h));
 	return (img);
 }
