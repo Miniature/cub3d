dd13d60d957e36aa17f292aabd4f28e980e6c733
Mini <wndavey@hotmail.com>
2024-02-26 15:32:52 +1030

    re:re:re fix rotate again

--- src/utils/pos_rotate.c
+++ src/utils/pos_rotate.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 15:44:13 by wdavey            #+#    #+#             */
-/*   Updated: 2024/02/01 15:49:28 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/02/26 14:52:04 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ t_pos	pos_rotate(t_pos pos, float angle)
 {
 	return ((t_pos){
 		pos.x * cos(angle) + pos.y * sin(angle),
-		pos.y * cos(angle) + pos.x * sin(angle)
+		pos.y * cos(angle) - pos.x * sin(angle)
 	});
 }
