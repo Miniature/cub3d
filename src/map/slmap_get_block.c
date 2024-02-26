71c8f9cd3e753eaa588ef6bad3bf495090937d0c
Mini <wndavey@hotmail.com>
2024-02-26 15:32:40 +1030

    dont crash on out of bounds

--- src/map/slmap_get_block.c
+++ src/map/slmap_get_block.c
@@ -3,19 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   slmap_get_block.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ihogben <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 14:03:38 by ihogben           #+#    #+#             */
-/*   Updated: 2024/02/08 14:03:46 by ihogben          ###   ########.fr       */
+/*   Updated: 2024/02/26 15:31:42 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "slmap.h"
 
+#include "libft.h"
 #include <stdlib.h>
 
 //takes an x and y input and returns the map data at that position
-char	slmap_get_block(t_slmap map, int x, int y)
+char	slmap_get_block(t_slmap map, size_t y, size_t x)
 {
-	return (map.raw[x][y]);
+	if (y >= map.height || x >= (size_t)ft_strlen(map.raw[y]))
+		return ('1');
+	return (map.raw[y][x]);
 }
