99742085cf99bb2ab1e9845973b1b3f81e6d5e67
Mini <wndavey@hotmail.com>
2024-03-04 19:56:46 +1030

    load img dataaddr immediately

--- src/mlxw/mlxw_load_xpm.c
+++ src/mlxw/mlxw_load_xpm.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 14:30:40 by wdavey            #+#    #+#             */
-/*   Updated: 2023/08/30 13:46:02 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/03/04 19:42:59 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ t_mlx_image	mlxw_load_xpm(void *mlx, char *path)
 	t_mlx_image	img;
 
 	img.img = mlx_xpm_file_to_image(mlx, path, &(img.w), &(img.h));
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
 	return (img);
 }
