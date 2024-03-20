baa563575c641d650eece721197be6406ddfe0f5
Mini <wndavey@hotmail.com>
2024-03-20 20:49:12 +1030

    fix crash on exit

--- src/gamestate/gamestate_destroy.c
+++ src/gamestate/gamestate_destroy.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 16:53:15 by wdavey            #+#    #+#             */
-/*   Updated: 2023/09/02 14:27:40 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/03/20 20:48:05 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,6 @@ void	gamestate_destroy(t_gamestate state, t_mlx_window win)
 	ft_lstclear(&entities, free);
 	sprite_delete(&(state.terrain_sprites.floor), win.mlx);
 	sprite_delete(&(state.terrain_sprites.wall), win.mlx);
-	mlxw_destroy_image(state.terrain, win.mlx);
+	//mlxw_destroy_image(state.terrain, win.mlx);
 	slmap_delete(state.map);
 }
